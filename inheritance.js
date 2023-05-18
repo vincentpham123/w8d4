0e13037563c6a64f2b37f0fa712589f86642e565
a-wong-8 <awong88@berkeley.edu>
2023-05-18 16:02:07 -0700

    .

--- inheritance.js
+++ inheritance.js
@@ -1,8 +1,8 @@
 Function.prototype.inherits = function(parent) {
     function Surrogate() {};
-    Surrogate.prototype = parent.prototype;
+    Surrogate.prototype = parent.prototype; // bio passed to surrogate proto
+    this.prototype = new Surrogate() // child set to instance 
     this.prototype.constructor = this;
-    this.prototype = new Surrogate()
 }
 
 function Parent(name, age) {
@@ -20,8 +20,9 @@ function Child(name, age) {
     Parent.call(this, name, age);
 }
 
-Child.inherits(Parent)
+Child.inherits(Parent);
+
 const test2 = new Child('Anyting', 10);
 test2.bio()
 
-console.log(Child.prototype.constructor);
+console.log(test2.__proto__.__proto__.__proto__);
